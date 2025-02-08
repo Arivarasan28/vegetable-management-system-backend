bceec4a0042db5a606a09b44b839a01030c67971
Arivarasan28 <jarivarasan2000@gmail.com>
2025-02-08 10:33:28 +0530

    initial create

---
+++ src/main/java/com/vms/vegetable_management_system_backend/VegetableManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vms.vegetable_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableManagementSystemBackendApplication.class, args);
+	}
+
+}
